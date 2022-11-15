e778b52a2766a67ecfa8088e67bfc9bce8ad35a7
Gulsenem <Gulsenem@users.noreply.github.com>
2022-11-15 11:11:59 +0000

    deploy: c44938e6db1bdca028a88fdc4610f45e68867e5f

---
+++ assets/Profile.2505e40a.js
@@ -0,0 +1,1 @@
+import{T as t,o as a,c as r,t as s,u as o}from"./index.c989ebfe.js";const u={__name:"Profile",setup(n){const{user:e}=t();return(c,l)=>(a(),r("p",null,s(o(e)),1))}};export{u as default};
